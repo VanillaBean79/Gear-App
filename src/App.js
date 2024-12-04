218e49be7f180cf9ba4946e08104204ab2a86f8a
Ruben Salazar <rubensalazar@rocketmail.com>
2024-12-04 11:29:53 -0600

    GearPage component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
+import GearPage from './GearPage'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-     Jam!
+    <GearPage/>
     </div>
   );
 }
---
+++ src/GearPage.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+function GearPage() {
+    return (
+        <div className="GearPage">
+            Gear Control
+        </div>
+    )
+}
+
+export default GearPage
