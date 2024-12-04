38103182395b3fc987486a90234508df51676b12
Ruben Salazar <rubensalazar@rocketmail.com>
2024-12-04 14:08:23 -0600

    Gear Manager

--- src/GearPage.js
+++ src/GearPage.js
@@ -1,9 +1,24 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
+
 
 function GearPage() {
+    const [gearData, setGearData] = useState([])
+    const [loading, setLoading] = useState(true)
+
+   useEffect(() => {
+        fetch('http://localhost:3001/Gear')
+          .then((response) => response.json())
+          .then((data) => {
+            console.log(data);
+          })
+          .catch((error) => {
+            console .error('Error fetching data:', error);
+          });
+      }, []);  
+
     return (
         <div className="GearPage">
-            Gear Control
+            Gear Manager
         </div>
     )
 }
