2550c46e91ef0eb39b07393c810b179c7a74a38c
Ruben Salazar <rubensalazar@rocketmail.com>
2024-12-05 11:32:12 -0600

    updated data/db.json

--- src/GearPage.js
+++ src/GearPage.js
@@ -6,19 +6,37 @@ function GearPage() {
     const [loading, setLoading] = useState(true)
 
    useEffect(() => {
-        fetch('http://localhost:3001/Gear')
+        fetch('http://localhost:3000/Gear')
           .then((response) => response.json())
           .then((data) => {
-            console.log(data);
+            setGearData(data)
+            setLoading(false);
           })
           .catch((error) => {
             console .error('Error fetching data:', error);
+            setLoading(false)
           });
       }, []);  
 
     return (
         <div className="GearPage">
             Gear Manager
+            <ul>
+                {gearData.map((gearItem)=> (
+                    <div className="card" key={gearItem.id}>
+                        <div className="card-content">
+                            <img 
+                              src={gearItem.image}
+                              alt={gearItem.name}
+                              className="card-image"
+                              />
+                           <h3 className="card-title">{gearItem.name}</h3>
+                           <p className="card-description">{gearItem.description}</p>
+                          <p className="card-serial">{gearItem.serial}</p>
+                      </div>
+                    </div>
+                ))}
+            </ul>
         </div>
     )
 }
