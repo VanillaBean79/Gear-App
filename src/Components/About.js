69d0e50068e2c5601b264cab310b24d4523ba1df
Ruben Salazar <rubensalazar@rocketmail.com>
2024-12-13 13:17:54 -0600

    image added to About page.

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import './About.css'
-
+import inst2 from '../images2/inst2.png'
 function About() {
     return (
         <div>
             <h2>About Gear Manager</h2>
             <p>This application allows you to manage your gear collection with ease. You can add new gear, view all gear, and more!</p>
+            <div className="About-image-container">
+                <img src={inst2} alt="About Image" />
+            </div>
         </div>
     );
 }
