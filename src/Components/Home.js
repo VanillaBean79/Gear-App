efb5cfaa319261ef6e00be75674ba5c52a847035
Ruben Salazar <rubensalazar@rocketmail.com>
2024-12-13 11:48:08 -0600

    image added to Home page.

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,5 +1,6 @@
 import React from "react";
-import './Home.css'
+import './Home.css';
+import instruments from '../images2/Instruments.png' 
 
 const Home = () => {
     return(
@@ -7,6 +8,9 @@ const Home = () => {
             <h1>My Gear Inventory</h1>
             <hr/>
             <p>This application stores all vital gear information. Upload a photo, type in the name, add a description, and the serial number for each piece of equipment that you would like to keep an inventory of for your personal records.</p>
+            <div className="home-image-container">
+                <img src={instruments} alt="Gear Managment" className="home-image" />
+            </div>
         </div>
     )
 }
